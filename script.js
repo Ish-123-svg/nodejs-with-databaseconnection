daae40e6af40c7e76e8df033b59387680817ad50
Ish-123-svg <85768180+Ish-123-svg@users.noreply.github.com>
2023-08-15 00:54:45 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// Connect to the database
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+// Example API route
+app.get('/data', (req, res) => {
+    db.query('SELECT * FROM your_table', (err, results) => {
+        if (err) {
+            console.error('Error querying the database:', err);
+            return res.status(500).json({ error: 'Database error' });
+        }
+        res.json(results);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
